69ee503aa60e2a884a9a0b99c43995c6543fb7be
SebastianGoebel <s.goebel@paradatec.de>
2016-11-10 15:36:00 +0100

    Code maintenance

--- core/src/zxing/pdf417/detector/LinesSampler.cpp
+++ core/src/zxing/pdf417/detector/LinesSampler.cpp
@@ -36,13 +36,6 @@ using zxing::Point;
 // VC++
 using zxing::Line;
 
-#if 0
-const int LinesSampler::MODULES_IN_SYMBOL;
-const int LinesSampler::BARS_IN_SYMBOL;
-const int LinesSampler::POSSIBLE_SYMBOLS;
-const int LinesSampler::BARCODE_START_OFFSET;
-#endif
-
 namespace {
 
 class VoteResult {
