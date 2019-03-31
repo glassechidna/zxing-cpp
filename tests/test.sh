cf29c473743c1a5f9b6b44b6a81afc619727e299
Nick <nick@kousu.ca>
2019-03-31 18:57:07 -0400

    Tidy tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -ex
 
 cd "$(dirname "$0")";
 
-for file in $(ls *.png); do
+find . -name "*.png" | while read file; do
   #zxing has a --test-mode but it's not working for binary files
-  zxing "$file" | diff $(ls "${file%.*}".{txt,bin} 2>/dev/null) -
+  # perl is here because in textmode, zxing appends a newline, but a lot of test files don't have that
+  zxing "$file" | perl -pe 'chomp if eof' | diff -u "$(ls "${file%.*}".{txt,bin} 2>/dev/null)" -
 done
